a43e90b01b0c7a12fd8f963f898028c7ca79a003
alessandrovaru <alessandrovaru@gmail.com>
2021-02-14 17:24:35 -0400

    Last push fixed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,17 @@
 class ArticlesController < ApplicationController
-    
+
+    before_action :find_article, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @articles = Article.all        
+    end
+
     def show
-        @article = Article.find(params[:id])
+        
     end
 
     def new 
-        #Nueva instancia del objeto con todos los campos vacios
+        
         @article = Article.new
     end
 
@@ -15,25 +21,21 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
+        
     end
-
+    
     def update
-        @article = Article.find(params[:id])
-        
         @article.update(title: params[:article][:title], content: params[:article][:content]) 
-        
         redirect_to @article
     end
 
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy
         redirect_to root_path
     end
 
-    def index
-        @articles = Article.all        
+    def find_article
+        @article = Article.find(params[:id])
     end
 
     
